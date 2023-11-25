d03ff49c1b4c95fd29d973a4edf4ba20dad31f63
smartdriver321 <tinggaldidisneyland@gmail.com>
2023-11-25 08:48:20 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000!');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
