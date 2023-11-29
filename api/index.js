f4a6f21df65868f8d0feddc14bd7434061a76b7b
smartdriver321 <tinggaldidisneyland@gmail.com>
2023-11-29 16:26:44 +0700

    create update user api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import userRoute from './routes/user.route.js';
 import authRoute from './routes/auth.route.js';
 
@@ -9,7 +9,7 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
 
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
