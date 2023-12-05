3dc3b1eee01b3026c49cd55c2e9580a8c6e4cfc0
smartdriver321 <tinggaldidisneyland@gmail.com>
2023-12-05 19:58:20 +0700

    complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -18,10 +18,12 @@ export default function Header() {
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const searchTermFromUrl = urlParams.get('searchTerm');
+
     if (searchTermFromUrl) {
       setSearchTerm(searchTermFromUrl);
     }
   }, [location.search]);
+
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
