a1ce2c3f7d3e66d433476a7372dab8be899c4e0b
smartdriver321 <tinggaldidisneyland@gmail.com>
2023-12-06 18:32:46 +0700

    add show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -255,7 +255,7 @@ export default function Search() {
               onClick={onShowMoreClick}
               className='text-green-700 hover:underline p-7 text-center w-full'
             >
-              Show more
+              Show more...
             </button>
           )}
         </div>
