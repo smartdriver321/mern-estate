32f594b4864574ba349cfbf7719940ecc96fb360
smartdriver321 <tinggaldidisneyland@gmail.com>
2023-12-06 19:28:34 +0700

    add pre installation

--- api/index.js
+++ api/index.js
@@ -12,12 +12,15 @@ mongoose
   .connect(process.env.MONGO)
   .then(() => {
     console.log('Connected to MongoDB!');
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000!');
+    });
   })
   .catch((err) => {
     console.log(err);
   });
 
-  const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -25,20 +28,15 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
-
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
-
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
